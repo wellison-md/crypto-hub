0310a29bf6eb9335719eb5cb8731b869e7aadb1e
wellison m. dias <wellisonmdias@gmail.com>
2024-01-23 18:11:30 -0300

    :zap: feat: add more attributes to Flex-comp

--- src/components/flex/flexStyle.jsx
+++ src/components/flex/flexStyle.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   align-items: ${ (props) => props.align ? props.align : 'center' };
+  background-color: ${ (props) => props.bg ? props.bg : 'transparent' };
   display: flex;
   flex-direction: ${ (props) => props.orientation ? props.orientation : 'row' };
   flex-wrap: wrap;
