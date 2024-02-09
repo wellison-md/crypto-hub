d3a3074ebcb23ec4431d4f045e58d3ef4e657ecf
wellison m. dias <wellisonmdias@gmail.com>
2024-02-08 22:03:52 -0300

    :zap: feat: remove logs

--- src/components/coinDetails/infoCoin/infoCoinHelpers.js
+++ src/components/coinDetails/infoCoin/infoCoinHelpers.js
@@ -1,10 +1,9 @@
 export function limitText(txt) {
   if (!txt || typeof txt !== 'string') {
-    return 'This coin don\'t provide a valid description yet';
+    return 'This coin don\'t provide a valid description yet\n';
   }
 
   const text = txt.split('.')
   const limitText = [...text].filter((_c, i) => i <= 3);
-  console.log(limitText.toString())
   return limitText.join('').toString();
 }
